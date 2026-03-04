dc88dc45a0be5821637ce399dad3a09447cc8c7c
Marek Trybuła <80851501+Trybuu@users.noreply.github.com>
2026-03-04 20:36:38 +0100

    Cu0003 header (#2)
    * feature: add header
    
    * feature: update changelog

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Header from './components/Header/Header'
 import Counter from './components/Counter/Counter'
 
 function App() {
   return (
     <main>
+      <Header />
       <h1>Welcome to React with TypeScript!</h1>
       <Counter />
     </main>
